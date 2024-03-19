96656a428ddeccdba7fb6707eb0b51b2d23b1cbf
noeticdigital <71247318+noeticdigital@users.noreply.github.com>
2024-03-19 16:10:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ def write_concept(product_name, company_name, use_case, product_type, needs, tar
 
 
 st.header("Noetic Digital - Startup Concept Generator:")
-st.write("Complete the form below and Noetic will generate your concept")
+st.write("Complete the form below and Noetic will generate your concept description")
 
-with st.form("Product/Service Concept Generator", clear_on_submit=False):
-    product_name = st.text_input("Product/Service Name: ")
-    company_name = st.text_input("Company name: ")
+with st.form("Product or Service Concept Generator", clear_on_submit=False):
+    product_name = st.text_input("Product or Service Name: ")
+    company_name = st.text_input("Company Name: ")
     use_case = st.text_area("Enter the product or service use case:")
     product_type = st_tags(
-        label="Enter your product type:",
+        label="Enter the product type:",
         text="Press enter after each product type",
         value=[],
         suggestions=["Saas", "food", "beverage", "appliance", "AI"],
@@ -50,19 +50,19 @@ with st.form("Product/Service Concept Generator", clear_on_submit=False):
         key="1"
     )
     target_user = st_tags(
-        label="Enter target user",
+        label="Enter the target user",
         text="Press enter after each user type",
         value=[],
         suggestions=["elderly", "caretakers", "families", "care homes", "doctors"],
         maxtags=8,
         key="2"
     )
-    needs = st.text_area("What specific needs does it meet?")
+    needs = st.text_area("What specific need/s does it meet?")
     new = st.text_area("What is new about this product?")
-    cred = st.text_area("Who produced it, including their background, history, and ‘right’ to make such a product?")
-    differentiation = st.text_area("What is different compared to other products that try to meet the same needs?")
-    promise = st.text_area("What is the core promise to the consumer i.e., what will this product do for me, and how will I feel?")
-    backup = st.text_area("What information can you give me to help me believe that?")
+    cred = st.text_area("Who produced it, including their background, history; and ‘right’ to make such a product?")
+    differentiation = st.text_area("What is different, when compared with other products that try to meet the same needs?")
+    promise = st.text_area("What is the core promise to the consumer i.e. what will this product do for me, and how will I feel?")
+    backup = st.text_area("What information can you offer to help me believe this promise?")
 
     submitted = st.form_submit_button("Write Concept")
     if submitted:
