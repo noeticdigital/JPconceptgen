819100d70782259f18bdde73a12be4c360182b8e
noeticdigital <71247318+noeticdigital@users.noreply.github.com>
2024-03-29 14:18:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import streamlit as st
 import streamlit_ext as ste
 import openai
 from streamlit_tags import st_tags
-
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 openai.api_key = OPENAI_API_KEY
-
 def write_concept(product_name, company_name, use_case, product_type, needs, target_user, new, cred, differentiation, promise, backup):
     try:
         response = openai.Completion.create(
             engine="gpt-3.5-turbo-instruct",  # Use "engine" instead of "model" for GPT-3.5-turbo
             prompt=(
-                f"write a 300 word product concept called {product_name}, created by {company_name} for {target_user}, "
-                f"include {needs}, {cred}, {new}, {differentiation}, {product_name}, {promise}, {backup}. "
+                f"write a product concept called {product_name}, created by {company_name} for {target_user}, "
+                f"the product solves {needs} because we have {cred}. "
+                f"The new point is {new} and the differentiation is {differentiation}. "
+                f"{product_name} promises to {promise} because {backup}. "
                 f"Here's an example concept -- Comfort Linen: Revolutionizing Rest for Those with Mobility Challenges. "
                 "Are you tired of getting tangled in your bedsheets, especially if you face pain or mobility issues? For millions worldwide, this is more than just an annoyance; it's a significant problem. Comfort Linen is the solution. It's a game-changing approach to your nightly routine. "
                 "After extensive research, we created an award-winning system of friction-reducing sheets initially for caregivers. However, inspired by patients seeking the same comfort at home, we expanded our innovation to offer a complete range of mobility-enhancing products for consumers. "
@@ -22,7 +22,7 @@ def write_concept(product_name, company_name, use_case, product_type, needs, tar
                 "Let's support the aging population, empower caregivers, and contribute to Japan's legacy of fostering health and wellness. Comfort Linen – where innovation meets tranquility, transforming the way we rest, one night at a time."
             ),
             temperature=0.7,
-            max_tokens=3080,
+            max_tokens=278,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
@@ -30,11 +30,8 @@ def write_concept(product_name, company_name, use_case, product_type, needs, tar
         return response['choices'][0]['text']
     except Exception as e:
         return f"An error occurred: {e}"
-
-
 st.header("Noetic Digital - Product or Service Concept Generator:")
 st.write("Complete the form below and Noetic will generate your concept description")
-
 with st.form("Product or Service Concept Generator", clear_on_submit=False):
     product_name = st.text_input("Product or Service Name: ")
     company_name = st.text_input("Company Name: ")
@@ -47,6 +44,9 @@ with st.form("Product or Service Concept Generator", clear_on_submit=False):
         maxtags=8,
         key="1"
     )
+
+    
+  
     target_user = st_tags(
         label="Enter the target user",
         text="Press enter after each user type",
@@ -61,7 +61,6 @@ with st.form("Product or Service Concept Generator", clear_on_submit=False):
     differentiation = st.text_area("What is different, when compared with other products that try to meet the same needs?")
     promise = st.text_area("What is the core promise to the consumer; i.e. what will this product do for me and how will I feel?")
     backup = st.text_area("What information can you offer to help me believe this promise?")
-
     submitted = st.form_submit_button("Write Concept")
     if submitted:
         with st.spinner("Writing concept..."):
